edc56f50866e2dc615b8e3749fe72317b265c9d1
Tim Brandin <tim.brandin@gmail.com>
2014-05-23 11:18:47 +0200

    Fixed routes.

--- client/router.js
+++ client/router.js
@@ -1,15 +1,15 @@
 Router.map(function() {
 
   this.route('home', {
-
+    path: '/'
   });
 
   this.route('project', {
-
+    path: '/project/:project'
   });
 
   this.route('func', {
-
+    path: '/project/:project/:function'
   });
 
 });
